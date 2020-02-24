727561831371a0615e4241aa017da7bdc6473136
Erik Örjehag <erik@orjehag.se>
2020-02-24 22:26:05 +0100

    Begin loss unsuperpoint

--- networks/unsuperpoint.py
+++ networks/unsuperpoint.py
@@ -40,7 +40,7 @@ class UnsuperPoint(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.N = 500
+        self.N = 300
 
         self.backbone = nn.Sequential(
             bb_conv(3, 32),
@@ -90,13 +90,68 @@ class UnsuperPoint(nn.Module):
         Smax, ids = torch.topk(Sflat, k=self.N, dim=1, largest=True, sorted=False)
         Pmax = torch.stack([Pflat[i,:,ids[i]] for i in range(ids.shape[0])], dim=0)
         Fmax = torch.stack([Fflat[i,:,ids[i]] for i in range(ids.shape[0])], dim=0)
+
+        outputs = {
+            "S": Smax,
+            "P": Pmax,
+            "F": Fmax
+        }
+
+        return outputs
+
+class SiameseUnsuperPoint(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.unsuperpoint = UnsuperPoint()
+
+    def forward(self, data):
+        A = self.unsuperpoint(data["original"])
+        B = self.unsuperpoint(data["transformed"])
+        outputs = {
+            "A": A,
+            "B": B
+        }
+        return outputs
         
 class UnsuperLoss():
 
     def __init__(self):
-        pass
+        self.CORR_DIST = 8
 
     def __call__(self, data):
+        AP = data["A"]["P"]
+        AS = data["A"]["S"]
+        AF = data["A"]["F"]
+        BP = data["B"]["P"]
+        BS = data["B"]["S"]
+        BF = data["B"]["F"]
+
+        C = (AP.permute(0,2,1).unsqueeze(2) - BP.permute(0,2,1).unsqueeze(1)).norm(dim=-1)
+
+        Cmin, ids = torch.min(C, dim=1)
+        mask = Cmin.le(self.CORR_DIST)
+
+        d = Cmin[mask]
+        dmean = d.mean()
+
+        loss_position = d.sum()
+        
+        #BS_ = torch.stack([BS[i,ids[i]] for i in range(ids.shape[0])], dim=0)
+        #loss_score = ((AS - BS_)[mask] ** 2).sum()
+
+        mask = mask.view(-1)
+        ids = ids.view(-1)
+        AS_ = AS.view(-1)[mask]
+        BS_ = BS.view(-1)[ids][mask]
+
+        loss_score = (AS_ - BS_) ** 2
+
+        S_ = (AS_ + BS_) / 2
+        loss_usp = S_ * (d - dmean)
+
+        x = 1
+
         return 0, {}
 
 def main():
@@ -106,11 +161,14 @@ def main():
         dataset, batch_size=4, shuffle=False,
         num_workers=1, pin_memory=True)
 
-    model = UnsuperPoint()
+    loss_fn = UnsuperLoss()
+
+    model = SiameseUnsuperPoint()
     model.train()
 
     for data in loader:
-        output = model.forward(data["tgt"])
+        output = model.forward({ "original": data["tgt"], "transformed": data["tgt"] })
+        loss, debug = loss_fn(output)
 
 if __name__ == "__main__":
     multiprocessing.set_start_method('spawn', True)
